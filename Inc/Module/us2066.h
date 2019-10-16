64b0fcee4092d7a0aad851af3fd21d66bb522ec2
Chang Youb <mystyle1057@gmail.com>
2019-10-16 09:12:31 +0900

    back up

--- Inc/Module/us2066.h
+++ Inc/Module/us2066.h
@@ -4,34 +4,102 @@
 extern "C" {
 #endif
 
-/* I2C Interface Info */
-#define SLAVE2W                 0x3CU   /* 0,1,1,1,1,0,SA0,R/W# */
-#define DATA_BYTE_COMMAND       0x00    /* Control Byte : Co(Continuation bit) = 0, D/C#(Data/Command Selection bit = 0 */
-#define DATA_BYTE_DATA          0x40    /* Control Byte : Co(Continuation bit) = 0, D/C#(Data/Command Selection bit = 1 */
+/* @brief NHD-0420CW-AW3 Spec */
+#define ROW_N                       4
 
-#define I2C_TX_TIME_OUT         100
+/* @brief I2C Interface Info */
+#define I2C_TX_TIME_OUT             100
+#define SLAVE2W                     0x3CU   /* @brief 0,1,1,1,1,0,SA0,R/W# */
+#define DATA_BYTE_COMMAND           0x00    /* @brief Control Byte : Co(Continuation bit) = 0, D/C#(Data/Command Selection bit = 0 */
+#define DATA_BYTE_DATA              0x40    /* @brief Control Byte : Co(Continuation bit) = 0, D/C#(Data/Command Selection bit = 1 */
 
-#define CMD_DATA_SIZE   3
+/* @brief Fundamental Command Set */
+#define CLEAR_DISPLAY               0x01U   /* @brief Clear Display: Write "20H" to DDRAM and set DDRAM address to "00H" from AC */
+#define SET_DDRAM_ADDRESS           0x80U   /* @brief Set DDRAM address in address counter */
 
-#define START_BYTE      0xF8U
-#define START_BYTE_DC   0xFAU
+/* @brief Extended Command Set */
+#define FUNCTION_SELECTION_A        0x71U   /* @brief Function Selection A */
+#define FUNCTION_SELECTION_B        0x72U   /* @brief Function Selection B */
+#define FUNCTION_SELECTION_C        0xDCU   /* @brief Function Selection C */
+#define OLED_CHARACTERIZATION       0x78U   /* @brief SD=0b: OLED command set is disabled (POR) */
+#define OLED_CHARACTERIZATION_SD    0x79U   /* @brief SD=1b: OLED command set is enabled        */
 
-#define FUNCTION_SELECTION_A    0x71U
-#define FUNCTION_SELECTION_B    0x72U
+/* @brief Function select A */
+#define DISABLE_INTERNAL_VDD        0x00U   /* @brief Disable internal Vdd regulator at 5V I/O application mode */
+#define ENABLE_INTERNAL_VDD         0x5CU   /* @brief Enable internal Vdd regulator at 5V I/O application mode  */
+
+/* @brief RE=0 Set display/cusor/blink ON/OFF */
+#define DISPLAY_CTRL                0x08U   /* @brief D = 0:display OFF, C = 0:cusor OFF, B = 0:blink OFF */
+#define DISPLAY_ONOFF_CONTROL       0x04U   /* @brief D = 1: display ON     */
+#define CUSOR_ONOFF_CONTROL         0x02U   /* @brief C = 1: cusor ON       */
+#define CUSOR_BLINK_ONOFF_CONTROL   0x01U   /* @brief B = 1: cusor blink ON */
+
+/* @brief OLED Command Set */
+#define CONTRAST_CONTROL            0x81U   /* @brief Set Contrast Control */
+#define DISPLAY_SET                 0xD5U   /* @brief Set Display Clock Divide Ratio/Oscillator Frequency */
+#define PHASE_LENGTH                0xD9U   /* @brief Set Phase Length */
+#define SEG_PINS_HARDWARE_CONFIG    0xDAU   /* @brief Set SEG Pins Hardware Configuration */
+#define DESELECT_LEVEL              0xDBU   /* Set Vcomh Deselect Level */
+
+/* @brief Contrast Control Command */
+#define CONTRAST_VALUE              0x7FU   /* @brief contrast=127 (default value) */
+
+/* @brief Phase Length Command */
+#define PHASE2                      0xF0U   /* PHASE2 = 15 */
+#define PHASE1                      0x01U   /* PHASE1 = 1  */
+
+/* @brief Deselcet Level Command */
+#define VCC_0_65                    0x00U   /* ~0.65 x Vcc */
+#define VCC_0_71                    0x10U   /* ~0.71 x Vcc */
+#define VCC_0_77                    0x20U   /* ~0.77 x Vcc */
+#define VCC_0_83                    0x30U   /* ~0.83 x Vcc */
+#define VCC_1                       0x40U   /* 1 x Vcc     */
+
+/* @brief Extended Function Set */
+#define FUNCTION_SET                0x08U   /* @brief FW = 0: 5-dot font width | B/W = 0: black/white inverting of cursor disable | NW = 0: 1-line or 2-line display mode */
+#define FUNCTION_SET_NW             0x01U   /* @brief NW = 1: 3-line or 4-line display mode */
+#define FUNCTION_SET_BW             0x02U   /* @brief B/W = 1: black/white inverting of cusor enable */
+#define FUNCTION_SET_FW             0x04U   /* @brief FW = 1: 6-dot font width */
+
+/* @brief Entry Mode Set (R=1 )*/
+#define ENTRY_MODE_SET              0x04U   /* @brief BDC = 0: COM31->COM0, BDS = 0: SEG99->SEG0 */
+#define ENTRY_MODE_BDC              0x02U   /* @brief BDC(Data shift direction of common) = 1: COM0->COM31  */
+#define ENTRY_MODE_BDS              0x01U   /* @btief BDS(Data shift direction of segment) = 1: SEG0->SEG99 */
+
+/* @brief Extended Command Set Data */
+#define CLOCK_FREQUENCY             0x70U   /* @brief ratio = 1, frequency = 7 (default values) */
+
+/* @brief OPR[D1:D0]: Select the character no. of character generator */
+#define CGROM_240_CGRAM_8           0x00U   /* @brief CGROM = 240, CGRAM = 8 */
+#define CGROM_248_CGRAM_8           0x01U   /* @brief CGROM = 248, CGRAM = 8 */
+#define CGROM_250_CGRAM_6           0x02U   /* @brief CGROM = 250, CGRAM = 6 */
+#define CGROM_256_CGRAM_0           0x03U   /* @brief CGROM = 256, CGRAM = 0 */
+
+/* @brief ROM[D3:D2]: Select character ROM */
+#define ROM_A                       0x00U   /* @brief ROM = A */
+#define ROM_B                       0x04U   /* @brief ROM = B */
+#define ROM_C                       0x08U   /* @brief ROM = C */
+#define ROM_INVALID                 0x0CU   /* @brief ROM = Invalid */
+
+/* OLED Command Set Configuration */
+#define ALTERNATIVE                 0x10U   /* @brief Alternative (odd/even) SEG pin configuration */
+#define SEG_LEFT_RIGHT_REMAP        0x20U   /* @brief Enable SEG Left/Right remap */
 
 #pragma pack(1)
-
+/**
+@struct _ST_FUCTION_SET
+@brief  Function set register 
+*/
 struct _ST_FUCTION_SET {
-    unsigned char D0:1;     /* REV:Reverse enable bit (1 = enable, 0 = nomal)            */
-    unsigned char D1:1;     /* RE:Extended function register enable = 1                  */
-    unsigned char D2:1;     /* BE:CGRAM data blink enable bit                            */
-    unsigned char D3:1;     /* N:Display line number control bit (0 = 1-line, 1 = 2-line */
-    unsigned char D4:1;     /* RESERVED */
-    unsigned char D5:1;     /* 1        */
-    unsigned char D6:1;     /* RESERVED */
-    unsigned char D7:1;     /* RESERVED */
+    unsigned char D0:1;     /* @var REV:Reverse enable bit (1 = enable, 0 = nomal)            */
+    unsigned char D1:1;     /* @var RE:Extended function register enable = 1                  */
+    unsigned char D2:1;     /* @var BE:CGRAM data blink enable bit                            */
+    unsigned char D3:1;     /* @var N:Display line number control bit (0 = 1-line, 1 = 2-line */
+    unsigned char D4:1;     /* @var RESERVED */
+    unsigned char D5:1;     /* @var 1        */
+    unsigned char D6:1;     /* @var RESERVED */
+    unsigned char D7:1;     /* @var RESERVED */
 };
-
 #pragma pack(4)
 
 typedef union _FUCTION_SET {
@@ -39,8 +107,10 @@ typedef union _FUCTION_SET {
     unsigned char value;
 }FUCTION_SET;
 
-void NHD_US2600Init(void);
-void NHD_US2600Test(void);
+void NHD_US2066Init(void);
+void NHD_US2066DisplayOn(void);
+void NHD_US2066DisplayOff(void);
+void NHD_US2066DisplayClear(void);
 
 #ifdef __cplusplus
 }
--- Src/Module/us2066.c
+++ Src/Module/us2066.c
@@ -1,3 +1,10 @@
+/******************************************************
+ * @file us2066.c
+ * @date 2019/10/14
+ * @author ChangYoub Lim (mystyle1057@gmail.com)
+ * @version 1.9
+ * @brief OLED DISPLAY NHD-0420CW US2066 Driver 
+ ******************************************************/
 #include "./Module/us2066.h"
 #include "main.h"
 
@@ -6,15 +13,89 @@ extern I2C_HandleTypeDef hi2c1;
 static void command(uint8_t);
 static void data(uint8_t);
 
-void NHD_US2600Test(void)
-{    
-    NHD_US2600Init();
+void NHD_US2066Init(void)
+{
+    FUCTION_SET funcSet;        
+    
+    if(ROW_N == 2 || ROW_N == 4)
+        funcSet.reg.D3 = 1;             /* @brief N = 1 Display 2/4 lines */
+    else
+        funcSet.reg.D3 = 0;             /* @brief N = 0 Display 1/3 lines */
+    funcSet.reg.D5 = 1;                 /* @brief D5 = 1 */ 
+    
+    funcSet.reg.D1 = 1;                 /* @brief RE = 1 */      
+    command(funcSet.value);             /* @brief Function set : extended command set (RE=1)   */
+    command(FUNCTION_SELECTION_A);      /* @brief Function selection A                         */ 
+    data(DISABLE_INTERNAL_VDD);         /* @brief Disablle internal Vdd regulator at 5V I/O mode (for disable 2.8V I/O) */
+
+    funcSet.reg.D1 = 0;                 /* @brief RE = 0 */
+    command(funcSet.value);             /* @brief Function set : fundamental command set (RE=0) (exit from extended command set) */    
+    command(DISPLAY_CTRL);              /* @brief Display OFF, Cusor OFF, Blink OFF (default values) */
+    
+    funcSet.reg.D1 = 1;                 /* @brief RE = 1 */
+    command(funcSet.value);
+    command(OLED_CHARACTERIZATION_SD);  /* @brief OLED command set enabled (SD=1) */
+    command(DISPLAY_SET);               /* @brief Set display clock divide ratio/oscillator frequency */
+    command(CLOCK_FREQUENCY);           /* @brief ratio=1, frequency=7 (default values) */
+
+    command(OLED_CHARACTERIZATION);     /* @brief OLED characterization: OLED command set disable (SD=0) (exit from OLED command set) */
+    if(ROW_N > 2)
+        command(FUNCTION_SET | FUNCTION_SET_NW);    /* @brief Extended function set (RE=1): 5-dot font B/W inverting disabled, 3/4 lines */
+    else
+        command(FUNCTION_SET);                      /* @brief Extended function set (RE=1): 5-dot font B/W inverting disabled, 1/2 lines */
+
+    command(ENTRY_MODE_SET | ENTRY_MODE_BDC);       /* @brief Entry Mode set (RE=1): COM0->COM31, SEG99->SEG0 */
+    
+    command(FUNCTION_SELECTION_B);      /* @brief Function selelction B */            
+    data(ROM_C | CGROM_250_CGRAM_6);    /* @brief ROM/CGRAM selection: ROM C, CGROM=250, CGRAM=6 */
+    
+    command(OLED_CHARACTERIZATION_SD);  /* @brief OLED characterization: OLED command set enabled (SD=1) */
+    command(SEG_PINS_HARDWARE_CONFIG);  /* @brief Set SEG pins hardware configuration */
+    command(ALTERNATIVE);
+
+    command(FUNCTION_SELECTION_C);      /* @brief Function selelction C */            
+    command(0x00);                      /* @brief Internal VSL, represents GPIO pin HiZ, input disabled */
+
+    command(CONTRAST_CONTROL);          /* @brief Set contrast control */
+    command(CONTRAST_VALUE);            /* @brief contrast = 127 */
+
+    command(PHASE_LENGTH);              /* @brief Set phase length: */
+    command(PHASE2|PHASE1);             /* @brief phase2=15, phase1=1 (default: 0x78) */
+
+    command(DESELECT_LEVEL);            /* @brief Set VCOMH deselect level: */
+    command(VCC_1);                     /* @brief VCOMH deselect level=1 x Vcc (default: 0x20=0,77 x Vcc) */
+
+    command(OLED_CHARACTERIZATION);     /* @brief OLED characterization: OLED command set disabled (SD=0) (exit from OLED command set) */
+
+    funcSet.reg.D1 = 0;                 /* @brief RE = 0 */
+    command(funcSet.value);             /* @brief Function set : fundamental command set (RE=0) (exit from extended command set) */  
+
+    command(CLEAR_DISPLAY);             /* @brief clear display */
+    HAL_Delay(2);
+
+    command(SET_DDRAM_ADDRESS);                     /* @brief set DDRAM adddress to 0x00 */
+    command(DISPLAY_CTRL | DISPLAY_ONOFF_CONTROL);  /* @brief display ON */
+
+    HAL_Delay(100);
+    
     return ;
 }
 
-void NHD_US2600Init(void)
+void NHD_US2066DisplayOn(void)
 {
-    
+    command(DISPLAY_CTRL | DISPLAY_ONOFF_CONTROL | CUSOR_ONOFF_CONTROL | CUSOR_BLINK_ONOFF_CONTROL);    
+    return ;
+}
+
+void NHD_US2066DisplayOff(void)
+{
+    command(DISPLAY_CTRL);
+    return ;
+}
+
+void NHD_US2066DisplayClear(void)
+{
+    command(CLEAR_DISPLAY);
     return ;
 }
 
@@ -25,6 +106,7 @@ static void command(uint8_t cmd)
     i2cBuffer[0] = DATA_BYTE_COMMAND;
     i2cBuffer[1] = cmd;
     
+    printf("OLED NHD command > 0x%02X\r\n", i2cBuffer[1]);
     HAL_I2C_Master_Transmit(&hi2c1, SLAVE2W, i2cBuffer, sizeof(i2cBuffer), I2C_TX_TIME_OUT);
     
     return ;
@@ -36,86 +118,10 @@ static void data(uint8_t data)
     
     i2cBuffer[0] = DATA_BYTE_DATA;
     i2cBuffer[1] = data;
-
+    
+    printf("OLED NHD data > 0x%02X\r\n", i2cBuffer[1]);
     HAL_I2C_Master_Transmit(&hi2c1, SLAVE2W, i2cBuffer, sizeof(i2cBuffer), I2C_TX_TIME_OUT);
     
     return ;
 }
-
-#ifdef SPI_VER
-#define VERSION_01
-
-extern SPI_HandleTypeDef hspi1;
-
-static void command(uint8_t);
-static void setBuffer(uint8_t *, uint8_t *, uint8_t);
-
-void NHD_US2600Test(void)
-{    
-    NHD_US2600Init();
-    return ;
-}
-
-void NHD_US2600Init(void)
-{
-    FUCTION_SET funcSet;
-    
-    funcSet.reg.D1 = 1;
-    funcSet.reg.D3 = 1;
-    funcSet.reg.D5 = 1;
-    
-    printf("Extended function register = 0x%x\r\n", funcSet.value);
-    
-    command(funcSet.value);
-    command(FUNCTION_SELECTION_A);
-    
-    return ;
-}
-
-static void command(uint8_t cmd)
-{
-    uint8_t txData[CMD_DATA_SIZE];
-    uint8_t rxData[CMD_DATA_SIZE];
-    uint8_t lowerData = 0;
-    uint8_t upperData = 0;
-
-    setBuffer(&lowerData, &upperData, cmd);
-    txData[0] = START_BYTE;
-    txData[1] = lowerData;
-    txData[2] = upperData;
-    
-    HAL_GPIO_WritePin(SPI1_NSS_GPIO_Port, SPI1_NSS_Pin, GPIO_PIN_RESET);           
-    HAL_SPI_TransmitReceive(&hspi1, txData, rxData, sizeof(txData), 100);    
-    HAL_GPIO_WritePin(SPI1_NSS_GPIO_Port, SPI1_NSS_Pin, GPIO_PIN_SET);
-    
-    return ;
-}
-
-#ifdef VERSION_01 
-static void setBuffer(uint8_t *lData, uint8_t *uData, uint8_t cmd)
-{        
-    *lData = ((cmd >> 4) | (cmd << 4)) & 0x0F;
-    *uData = ((cmd >> 4) | (cmd << 4)) & 0xF0;
-
-    return ;
-}
-#else
-static void setBuffer(uint8_t *lData, uint8_t *uData, uint8_t cmd)
-{        
-    for(int8_t i = 7; i >= 4; i--)
-    {
-        *lData |= (cmd & 0x01) << i;
-        cmd = cmd >> 1;        
-    }
-
-    for(int8_t i = 7; i >= 4; i--)
-    {
-        *uData |= (cmd & 0x01) << i;
-        cmd = cmd >> 1;        
-    }
-    
-    return ;
-}
-#endif
-#endif
 
